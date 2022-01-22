4b1474861d01b1bf98cdc971f112c66e39edb1c8
inokawa <48897392+inokawa@users.noreply.github.com>
2022-01-22 18:09:44 +0900

    Use vm

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
 const { promises: fs } = require("fs");
+const vm = require("vm");
 const expect = require("expect");
 const mock = require("jest-mock");
 const { describe, it, run, resetState } = require("jest-circus");
@@ -11,7 +12,9 @@ exports.runTest = async function (testFile) {
   };
   try {
     resetState();
-    eval(code);
+    const context = { describe, it, expect, mock };
+    vm.createContext(context);
+    vm.runInContext(code, context);
 
     const { testResults } = await run();
     testResult.testResults = testResults;
