373eae554f4660d98c3a50a5a9277ca8f1c948ff
inokawa <48897392+inokawa@users.noreply.github.com>
2022-01-22 17:40:46 +0900

    Implement assertion

--- index.mjs
+++ index.mjs
@@ -1,8 +1,9 @@
 import JestHasteMap from "jest-haste-map";
 import { cpus } from "os";
-import { dirname, join } from "path";
+import { dirname, join, relative } from "path";
 import { fileURLToPath } from "url";
 import { Worker } from "jest-worker";
+import chalk from "chalk";
 
 const root = dirname(fileURLToPath(import.meta.url));
 
@@ -24,8 +25,15 @@ const worker = new Worker(join(root, "worker.js"), {
 
 await Promise.all(
   Array.from(testFiles).map(async (testFile) => {
-    const testResult = await worker.runTest(testFile);
-    console.log(testResult);
+    const { success, errorMessage } = await worker.runTest(testFile);
+    const status = success
+      ? chalk.green.inverse.bold(" PASS ")
+      : chalk.red.inverse.bold(" FAIL ");
+
+    console.log(status + " " + chalk.dim(relative(root, testFile)));
+    if (!success) {
+      console.log("  " + errorMessage);
+    }
   })
 );
 
--- worker.js
+++ worker.js
@@ -2,6 +2,23 @@ const { promises: fs } = require("fs");
 
 exports.runTest = async function (testFile) {
   const code = await fs.readFile(testFile, "utf8");
-
-  return testFile + ":\n" + code;
+  const testResult = {
+    success: false,
+    errorMessage: null,
+  };
+  try {
+    const expect = (received) => ({
+      toBe: (expected) => {
+        if (received !== expected) {
+          throw new Error(`Expected ${expected} but received ${received}.`);
+        }
+        return true;
+      },
+    });
+    eval(code);
+    testResult.success = true;
+  } catch (e) {
+    testResult.errorMessage = e.message;
+  }
+  return testResult;
 };
