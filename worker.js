b5d0b884c709173762f5ad70cea24e53f390674e
inokawa <48897392+inokawa@users.noreply.github.com>
2022-01-22 17:55:02 +0900

    Implement describe

--- worker.js
+++ worker.js
@@ -8,11 +8,28 @@ exports.runTest = async function (testFile) {
     success: false,
     errorMessage: null,
   };
+  let testName;
   try {
+    const describeFns = [];
+    let currentDescribeFn;
+    const describe = (name, fn) => describeFns.push([name, fn]);
+    const it = (name, fn) => currentDescribeFn.push([name, fn]);
+
     eval(code);
+
+    for (const [name, fn] of describeFns) {
+      currentDescribeFn = [];
+      testName = name;
+      fn();
+
+      currentDescribeFn.forEach(([name, fn]) => {
+        testName += " " + name;
+        fn();
+      });
+    }
     testResult.success = true;
   } catch (e) {
-    testResult.errorMessage = e.message;
+    testResult.errorMessage = testName + ": " + e.message;
   }
   return testResult;
 };
