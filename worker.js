400dc826372b135cd1bd91a4a6bc5d05700f41f9
inokawa <48897392+inokawa@users.noreply.github.com>
2022-01-22 17:46:48 +0900

    Use jest-mock

--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 const { promises: fs } = require("fs");
 const expect = require("expect");
+const mock = require("jest-mock");
 
 exports.runTest = async function (testFile) {
   const code = await fs.readFile(testFile, "utf8");
